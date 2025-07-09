62da701d3d2f4effa24114799443599735f74750
mobasshir <mobasshirkhan9931@gmail.com>
2025-07-09 10:36:11 +0530

    compiled tsc into js

---
+++ 01_intro/intro.js
@@ -0,0 +1,4 @@
+var user = { name: "mobasshir", age: 10 };
+console.log("mobasshir");
+console.log(user.name);
+console.log(user.email);
---
+++ 01_intro/intro.ts
@@ -0,0 +1,5 @@
+const user={name:"mobasshir",age:10}
+
+console.log("mobasshir");
+console.log(user.name);
+console.log(user.email);
